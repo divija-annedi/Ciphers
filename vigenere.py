51dd42f96344bdcc8f8051f89e74e433f68c6cd6
Divija Annedi <72152507+divija-annedi@users.noreply.github.com>
2020-12-24 09:55:40 +0530

    Add files via upload

---
+++ vigenere.py
@@ -0,0 +1,74 @@
+def new_alph(ch):
+    ch = ch.lower()
+    alph = 'abcdefghijklmnopqrstuvwxyz'
+    new_alph = alph[alph.index(ch):] + alph[:alph.index(ch)]
+    return new_alph
+    
+   
+def encrypt(text, big_key):
+    res = ''
+    alph = 'abcdefghijklmnopqrstuvwxyz'
+    i = 1
+    for char in big_key:
+        new = new_alph(char)
+        for t in text:
+            if alph.count(t) == 1 :
+                res += new[alph.index(t)]
+                text = text[i:]
+                break
+            elif alph.count(t.lower()) == 1:
+                res += new[alph.index(t.lower())].upper()
+                text = text[i:]
+                break
+            else:
+                res += t
+                text = text[i:]
+                break
+            i += 1    
+    return res
+    
+    
+def decrypt(text, big_key):
+    res = ''
+    alph = 'abcdefghijklmnopqrstuvwxyz'
+    i = 1
+    for char in big_key:
+        new = new_alph(char)
+        for t in text:
+            if alph.count(t) == 1 :
+                res += alph[new.index(t)]
+                text = text[i:]
+                break
+            elif alph.count(t.lower()) == 1:
+                res += alph[new.index(t.lower())].upper()
+                text = text[i:]
+                break
+            else:
+                res += t
+                text = text[i:]
+                break
+            i += 1    
+    return res    
+    
+
+text1 = input("Enter plaintext:")
+text_dec = input("Enter ciphertext")
+# Put your key 
+key = input("Enter key")
+
+if len(key) <= len(text1):
+    big_key = key * (len(text1) // len(key)) + key[:len(text1) % len(key)]
+    text_encrypt = encrypt(text1, big_key)
+    text_decrypt = decrypt(text_dec, big_key)
+    
+   
+    print('|Your plaintext: "' + text1 + '"')
+    print('|Your key : "' + key + '"')
+    print('|Ciphertext     : ' + text_encrypt)
+    
+    print('|Your ciphertext: "' + text_dec + '"')
+    print('|Your key : "' + key + '"')
+    print('|Plaintext     : ' + text_decrypt)
+   
+else:
+    print('Error: len(key)>len(text) ')
